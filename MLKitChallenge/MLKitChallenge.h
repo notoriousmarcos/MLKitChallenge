45dee84ddac8a73c6a993979c5728fc06b25c992
Marcos Brito <marcosvcbrito@gmail.com>
2023-07-09 14:03:48 -0300

    Initial Commit

---
+++ MLKitChallenge/MLKitChallenge.h
@@ -0,0 +1,18 @@
+//
+//  MLKitChallenge.h
+//  MLKitChallenge
+//
+//  Created by Marcos Vinicius Brito on 09/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MLKitChallenge.
+FOUNDATION_EXPORT double MLKitChallengeVersionNumber;
+
+//! Project version string for MLKitChallenge.
+FOUNDATION_EXPORT const unsigned char MLKitChallengeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLKitChallenge/PublicHeader.h>
+
+
